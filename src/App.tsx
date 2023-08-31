f3705f70532060d1db5bed760f3ce966e81a4b86
Proprogam <k.kartal9595@gmail.com>
2023-08-31 16:02:31 +0300

    tmock data added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home/Home";
 import Turk from "./pages/Turk/Turk";
-import Math from "./pages/Math/Math";
+import Math from "./pages/math/Math";
+
 function App() {
   return (
     <>
