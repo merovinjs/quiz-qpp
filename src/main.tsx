673914371007341ffd282f3925b33164c407a793
Proprogam <k.kartal9595@gmail.com>
2023-09-01 17:42:45 +0300

    modal  added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import Example from "./example/Example.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
