0310b223dc5aefd592e3df6ca6844b0a679c1b71
Proprogam <k.kartal9595@gmail.com>
2023-09-06 14:14:23 +0300

    themeContext created

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,30 +1,21 @@
 import styles from "./styles.module.css";
 const Home = () => {
-  let colors: string[] = [
-    "#00bfff",
-    "#ff1493",
-    "#ff69b4",
-    "#ffd700",
-    "#ffa500",
-    "#ffc0cb",
-    "#00ff7f",
-    "#00fa9a",
-    "#00ff00",
-    "#008080",
-    "#0000ff",
-  ];
+  let colors: string[] = ["#3c787e", "#241623", "#d56f3e", "#92140c"];
 
   return (
-    <>
+    <div className={styles.home}>
       <div className={styles.container}>
-        <h1>Bursluluk Sınavı Çıkmış Sorular</h1>
+        <div className={styles.title}>
+          <h1>Bursluluk Sınavı Çıkmış Sorular</h1>
+          <img src="./paraanimasyon.gif" alt="" />
+        </div>
         <div className={styles.btnContainer}>
           <a style={{ backgroundColor: colors[0] }} href="/turk">
             Türkçe 2020 soruları
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
