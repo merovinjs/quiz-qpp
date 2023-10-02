a432eafab6a449cb915ee4ac5ff3ac36a581ec7d
Proprogam <k.kartal9595@gmail.com>
2023-10-02 15:27:56 +0300

    yanlış cevaplar component created

--- src/data/yanlışcvpdata.ts
+++ src/data/yanlışcvpdata.ts
@@ -8,5 +8,21 @@ export const yanlıscvpdata = {
       id: 2,
       question: "/02.10.23/ödev2.png",
     },
+    {
+      id: 3,
+      question: "/02.10.23/ödev3.png",
+    },
+    {
+      id: 4,
+      question: "/02.10.23/ödev4.png",
+    },
+    {
+      id: 5,
+      question: "/02.10.23/ödev5.png",
+    },
+    {
+      id: 6,
+      question: "/02.10.23/ödev6.png",
+    },
   ],
 };
